3fbe718957f5bb565323896ebe43746baeb0a1f4
Sebastiaan Verbeek <contact@svatwork.com>
2020-05-01 14:11:32 +0700

    feat: type text on screen

--- js/display.js
+++ js/display.js
@@ -1,10 +1,9 @@
 const outputDisplay = document.getElementById("outputDisplay");
-outputDisplay.innerHTML = "...";
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
         // console.log(event.target.textContent);
-        outputDisplay.innerHTML = event.target.textContent;
+        outputDisplay.innerHTML += event.target.textContent;
     });
 });
