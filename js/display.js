4e743a0620933b6c71e884d1f521e9a47421d6d3
Sebastiaan Verbeek <contact@svatwork.com>
2020-05-01 15:07:08 +0700

    feat: operator on two numbers

--- js/display.js
+++ js/display.js
@@ -8,7 +8,23 @@ buttons.forEach((button) => {
     });
 });
 
+const ops = document.querySelectorAll(".op");
+ops.forEach((button) => {
+    button.addEventListener('click', function (event) {
+        let op = this.id;
+        let a = parseInt(outputDisplay.innerHTML);
+        outputDisplay.innerHTML = "";
+
+        const evaluate = document.querySelector(".evaluate");
+        evaluate.addEventListener('click', (event) => {
+            const b = parseInt(outputDisplay.innerHTML);
+            outputDisplay.innerHTML = operate(op, a, b).toString(10);
+        });
+    });
+});
+
 const clear = document.querySelector(".clear");
 clear.addEventListener('click', (event) => {
     outputDisplay.innerHTML = "";
+    evaluation = 0;
 });
--- js/operators.js
+++ js/operators.js
@@ -13,3 +13,18 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
     return a / b;
 };
+
+const operate = (op, a, b) => {
+    switch (op) {
+        default:
+            break;
+        case "add":
+            return add(a, b);
+        case "subtract":
+            return subtract(a, b);
+        case "multiply":
+            return multiply(a, b);
+        case "divide":
+            return divide(a, b);
+    }
+}
